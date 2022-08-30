34fbcb2659514d08b1bff12c80d67198c98a31bd
The Amaral <82917923+AmaralRage@users.noreply.github.com>
2022-08-30 20:02:07 -0300

    Initial commit

---
+++ BD3.sql
@@ -0,0 +1,38 @@
+create database bd3;
+
+use bd3;
+
+create table alunos (
+	id int (11)
+	primary key auto_increment,
+	nome varchar(255) not null,
+    idade int (3),
+	nascimento date default null
+ );
+ 
+-- insert 
+insert into alunos (nome, nascimento) values ('leo', '2011-03-04');
+insert into alunos (nome, nascimento) values ('Amaral', '2012-03-04');
+insert into alunos (nome, nascimento) values ('Bruno', '2013-03-04');
+insert into alunos (nome, nascimento) values ('Giovani', null);
+insert into alunos (nome, nascimento) values ('Matheus', '2015-03-04');
+
+-- selecionar todos os registros
+select nome, nascimento from alunos;
+
+-- com filtro
+select nascimento from alunos
+where nascimento is null;
+
+-- filtrar os resultados que tenha o campo nascimento nulo
+select * from alunos
+where nascimento is null;
+
+-- lista de presenca - ordenada em ordem alfabetica
+select distinct nome
+from alunos
+order by nome asc;
+
+-- listar somente dois alunos
+select * from alunos
+limit 2;
---
+++ BD3MD4_2.sql
@@ -0,0 +1,23 @@
+use bd3;
+
+create table venda (
+		id int(11) primary key auto_increment,
+        produto varchar(255),
+        quantidade int(11),
+        data_venda date
+);
+
+insert into vendas (produto, quantidade, data_venda)
+values ('Audi TT', 2, '2022-06-10');
+insert into vendas (proudto, quantidade, data_venda)
+values ('BMW', 3, '2022-05-01');
+insert into vendas (produto, quantidade, data_venda)
+values ('Ferrari', 10, '2022-07-12');
+insert into vendas (produto, quantidade, data_venda)
+values ('Masserati', 1, '2022-08-22');
+insert into vendas (produto, quantidade, data_venda)
+values ('Bentley', 4, '2022-08-30');
+
+select * from vendas
+where
+quantidade between 3 and 9;
